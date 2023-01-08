b92500a0d1b34ebedabdccfc87e77aa9e6e12554
Kevin Grimaldi <anthgrim@icloud.com>
2023-01-08 12:44:31 -0500

    small change

--- server/server.js
+++ server/server.js
@@ -19,9 +19,9 @@ connectDb()
 
 // Middlewares
 app.use(express.json())
-app.use(express.urlencoded())
+app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
-// app.use(cors)
+// app.use(cors(--Needs cors options here--))
 
 // Routes
 app.use('/api/user', userRoutes)
