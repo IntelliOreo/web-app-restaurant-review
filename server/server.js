12f4f5de71c2cfa8ad4c824a58aa92ced9ef3e1c
Kevin Grimaldi <anthgrim@icloud.com>
2023-01-08 12:39:15 -0500

    clean up server

--- server/server.js
+++ server/server.js
@@ -1,46 +1,49 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const path = require('path');
+import express from 'express'
+import cookieParser from 'cookie-parser'
+import dotenv from 'dotenv'
+import connectDb from './data/connectDb.js'
+import mongoose from 'mongoose'
+import userRoutes from './routes/UserRoutes.js'
+import reviewRoutes from './routes/ReviewRoutes.js'
+import sessionRoutes from './routes/SessionRoutes.js'
+// import path from 'path'
+// import cors from 'cors'
+dotenv.config()
 
-const cookieController = require('./controllers/cookieController');
-const sessionController = require('./controllers/sessionController');
+// Instantiate Express App
+const app = express()
+const PORT = process.env.PORT
 
-const app = express();
-const PORT = 3000;
+// Connect to database
+connectDb()
 
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(cookieParser());
+// Middlewares
+app.use(express.json())
+app.use(express.urlencoded())
+app.use(cookieParser())
+// app.use(cors)
 
-app.get(
-  '/api',
-  cookieController.setCookie,
-  sessionController.startSession,
-  (req, res) => {
-    return res.json(res.locals.curSession);
-  }
-);
+// Routes
+app.use('/api/user', userRoutes)
+app.use('/api/review', reviewRoutes)
+app.use('/api/session', sessionRoutes)
 
-app.post('/api', sessionController.updateSession, (req, res) => {
-  return res.json(res.locals.updateSession);
-});
-
-app.delete('/api', sessionController.clearupSession, (req, res) => {
-  return res.sendStatus(200);
-});
-
+// Not Found
 app.use('*', (req, res) => {
-  res.status(404).send('Not Found - 404 handler in server.js');
-});
+  res.status(404).send('Not Found - 404 handler in server.js')
+})
 
 app.use((err, req, res, next) => {
-  console.log(err);
-  const defaultErr = { error: 'An error occur' };
-  const errObj = Object.assign({}, defaultErr, err);
-  console.log('errObj in global handler', errObj);
-  res.status(500).json(errObj);
-});
+  console.log(err)
+  const defaultErr = { error: 'An error occur' }
+  const errObj = Object.assign({}, defaultErr, err)
+  console.log('errObj in global handler', errObj)
+  res.status(500).json(errObj)
+})
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port: ${PORT}`);
-});
+// Listen only if db is connected
+mongoose.connection.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`Server listening on port: ${PORT}`)
+  })
+})
