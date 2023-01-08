9a1724995a8ee3e6c838f49afcb99998d02af4bc
Kevin Grimaldi <anthgrim@icloud.com>
2023-01-08 12:37:19 -0500

    Moved controller logic for session and cookie to a middleware folder

---
+++ server/middlewares/session.js
@@ -0,0 +1,17 @@
+import Session from '../models/sessionModel'
+
+const sessionController = {}
+
+sessionController.startSession = async (req, res, next) => {
+  return next()
+}
+
+sessionController.updateSession = (req, res, next) => {
+  return next()
+}
+
+sessionController.clearupSession = async (req, res, next) => {
+  return next(err)
+}
+
+export default sessionController
