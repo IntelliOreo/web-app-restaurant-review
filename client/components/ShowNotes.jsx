1448943a8e8ad41a0353930e64e742fd9b1fd050
Kevin Grimaldi <anthgrim@icloud.com>
2023-01-30 19:49:26 -0500

    show notes components and styles

--- client/components/ShowNotes.jsx
+++ client/components/ShowNotes.jsx
@@ -1,28 +1,28 @@
-import React, {useEffect, useState } from 'react'
-import "./ShowNotes.css"
-import axios from "axios"
-import NotesList from "./NotesList"
-import {toast} from 'react-toastify'
+import './ShowNotes.css'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import NotesList from './NotesList'
+import { toast } from 'react-toastify'
 
 const baseUrl = `http://localhost:8050/api/db`
 
 function ShowNotes() {
-  const [notes, setNotes] = useState([]);
- 
+  const [notes, setNotes] = useState([])
+
   // Load notes on mount
 
-  useEffect(()=>{
-    axios.get(`${baseUrl}`)
-    .then((res)=>{
-
-      console.log(Array.isArray(res.data))
-      setNotes(res.data);
-      console.log("notes " + notes);
-    })
-    .catch((err)=>{
-      console.log(err)
-    })
-  },[])
+  useEffect(() => {
+    axios
+      .get(`${baseUrl}`)
+      .then((res) => {
+        console.log(Array.isArray(res.data))
+        setNotes(res.data)
+        console.log('notes ' + notes)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }, [])
 
   // Client updates
   /**
@@ -30,14 +30,16 @@ function ShowNotes() {
    * @param noteId
    */
   const deleteNote = async (e, noteId) => {
-    e.preventDefault();
+    e.preventDefault()
     try {
       const res = await axios.delete(`${baseUrl}/${noteId}`)
       console.log(res)
 
       if (res.status === 200) {
-        const filterNotes = notes.filter(item => item.id !== noteId); 
-        const deletedNote = notes.find(deletedNote => deletedNote.id === noteId);
+        const filterNotes = notes.filter((item) => item.id !== noteId)
+        const deletedNote = notes.find(
+          (deletedNote) => deletedNote.id === noteId
+        )
         setNotes(filterNotes)
         toast.success('Note deleted succesfully')
       } else {
@@ -51,19 +53,17 @@ function ShowNotes() {
     }
   }
 
-
-   return (
-
-     <div className='background-notes'> 
-     {notes.map((note) => { 
-      return <div key={note.id} className = "body-note">
-      <NotesList note={note} deleteNote={deleteNote} /> 
-      </div>
-      })} 
+  return (
+    <div className='background-notes'>
+      {notes.map((note) => {
+        return (
+          <div key={note.id} className='body-note'>
+            <NotesList note={note} deleteNote={deleteNote} />
+          </div>
+        )
+      })}
     </div>
- )
+  )
 }
 
-export default  ShowNotes;
-
-
+export default ShowNotes
