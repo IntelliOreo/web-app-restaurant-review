fa925172e453dd271ff1ccdf90bd1161560021d6
E <g-1704@hotmail.com>
2023-03-05 00:17:00 -0500

    templete withiout my notes filtering by user id

--- client/components/showMyNotes.jsx
+++ client/components/showMyNotes.jsx
@@ -1,43 +1,70 @@
-import CreateNew from "./CreateNew";
-import NotesList from './myNotes'
 import './ShowNotes.css'
 import React, { useEffect, useState } from 'react'
+import CreateNew from './CreateNew'
 import axios from 'axios'
-
+import NotesList from './MyNotes'
+import { toast } from 'react-toastify'
 
 
 function ShowMyNotes() {
-
   const [notes, setNotes] = useState([])
 
   // Load notes on mount
 
-  const baseUrl = 'http://localhost:8080/api/note/show'
-
+  const url = 'http://localhost:8080/api/note/show'
   useEffect(() => {
     axios
-      .get(`${baseUrl}`)
+      .get(`${url}`)
       .then((res) => {
-        console.log(Array.isArray(res.data))
-        setNotes(res.data)
+        setNotes(res.data.notes)
       })
       .catch((err) => {
         console.log(err)
       })
   }, [])
 
-    return(
-      <div > 
-      {/* <CreateNew/> */}
+  // Client updates
+  /**
+   * @param e -> Event
+   * @param noteId
+   */
+  const deleteNote = async (e, noteId) => {
+    e.preventDefault()
+    try {
+      const res = await axios.delete(`${url}/${noteId}`)
+      console.log(res)
+
+      if (res.status === 200) {
+        const filterNotes = notes.filter((item) => item.id !== noteId)
+        const deletedNote = notes.find(
+          (deletedNote) => deletedNote.id === noteId
+        )
+        setNotes(filterNotes)
+        toast.success('Note deleted succesfully')
+      } else {
+        console.log('Note deleted on db')
+        toast.error('Unable to delete note on client')
+      }
+    } catch (e) {
+      console.error(e)
+      toast.error('Unable to delete note. Contact Administrator')
+      return
+    }
+  }
+
+  return (
+    <div className='background-notes'>
+      <CreateNew/>
+
       {notes.map((note) => {
         return (
           <div key={note.id} className='body-note'>
-            <NotesList note={note} />
+            <NotesList note={note} deleteNote={deleteNote} />
           </div>
         )
       })}
-      </div>
-    );
+    </div>
+  )
 }
 
-export default ShowMyNotes;
+export default ShowMyNotes
